fdd394bd662dc5e58da8ccc981a9bff185b58cb1
EllieKonuma <elliekonuma@gmail.com>
2022-07-30 23:43:03 -0300

    Refactor Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,46 +1,36 @@
-import { Box, Center, Flex, Icon, Text } from "@chakra-ui/react";
+import { Center, HStack, Icon, Text } from "@chakra-ui/react";
 import React from "react";
 
 import { TbGenderTransgender } from "react-icons/tb";
 
 function Banner() {
   return (
-    <Flex
-      fontSize={"5xl"}
-      bgGradient={"linear(to-r, blue.200, pink.200, pink.200, blue.200)"}
-      borderBottom={"1px solid white"}
-      w={"100%"}
-      h={"425px"}
-      mt={"75px"}
-      wrap={"wrap"}
-    >
-      <Flex
-        w={"50%"}
-        h={"100%"}
-        alignItems={"center"}
-        minWidth={"475px"}
-
-        // alignContent={"flex-start"}
+    <Center>
+      <HStack
+        w={"100%"}
+        h={"425px"}
+        mt={"75px"}
+        fontSize={"5xl"}
+        bgGradient={"linear(to-r, blue.200, pink.200, pink.200, blue.200)"}
+        justifyContent={"space-around"}
       >
-        <Text ml={"2em"} mb={"1em"} color={"white"} fontWeight={"extrabold"}>
-          Lugar seguro com informações confiáveis para pessoas trans
+        <Text
+          color={"white"}
+          fontWeight={"extrabold"}
+          ml={{ base: "1em", lg: "0" }}
+          mr={{ base: "1em", mg: "0" }}
+          maxW={"650px"}
+        >
+          Lugar seguro com informações confiaveis para pessoas trans.
         </Text>
-      </Flex>
-      <Flex
-        justifyContent={"center"}
-        w={"50%"}
-        h={"100%"}
-        alignItems={"center"}
-      >
         <Icon
           as={TbGenderTransgender}
-          mr={"1em"}
-          mb={"0.5em"}
           boxSize={"300"}
           color={"white"}
+          display={{ base: "none", md: "block" }}
         />
-      </Flex>
-    </Flex>
+      </HStack>
+    </Center>
   );
 }
 
